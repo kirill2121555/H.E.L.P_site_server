4786fba28a999ad634710c9ef7a0e6f717264bbb
kirill2121555 <kirill2121555@gmail.com>
2022-08-24 14:35:20 +0300

    init

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+const express = require('express')
+const app = express()
+const { config } = require('dotenv')
+require('dotenv').config()
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
 const path = require('path')
-const express = require('express')
-const router = require('../routers/router')
+
+const router = require('./routers/router')
 
 const cors = require('cors')
 const multer = require('multer')
 const cookieParser = require('cookie-parser')
 const errorMiddleware = require('./middlewares/error-middleware')
-const { config } = require('dotenv')
-require('dotenv').config()
 
 
-const app = express()
+
 
 app.use(express.json())
 app.use('/images',express.static('images'))
